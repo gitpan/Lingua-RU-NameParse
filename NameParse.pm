25b50bf45ff2cc1e436795cd66da20d45357fec2
Maciej Ceglowski <mceglows@middlebury.edu>
2004-03-09 14:24:41 +0000

    Import of MCEGLOWS/Lingua-RU-NameParse-0.02 from CPAN.
    gitpan-cpan-distribution: Lingua-RU-NameParse
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         MCEGLOWS/Lingua-RU-NameParse-0.02.tar.gz
    gitpan-cpan-author:       MCEGLOWS
    gitpan-cpan-maturity:     released

--- NameParse.pm
+++ NameParse.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 sub new { my $class = shift; 
@@ -48,6 +48,7 @@ sub normalize {
 	elsif ( $w2 =~ /овну\b/ ) {
 		$w2 =~ s/у\b/а/;
 		$w1 =~ s/ью\b/ья/;
+		$w1 =~ s/ию\b/ия/;
 	}
 	
 	################
@@ -55,6 +56,7 @@ sub normalize {
 	################
 	elsif (  $w2 =~ /не\b/ ) {
 		$w2 =~ s/е\b/а/;
+		$w1 =~ s/ье\b/ья/;
 		$w1 =~ s/е\b/а/;
 		$w1 =~ s/ии\b/ия/;
 	}
@@ -68,7 +70,7 @@ sub normalize {
 	elsif (  $w2 =~ /[нч]у$/ ) {
 		$w2 =~ s/у$//;
 		$w1 =~ s/у\b//;
-		$w1 =~ s/ию\b/ий/;
+		$w1 =~ s/([еи])ю\b/$1й/;
 	}
 	
 
@@ -81,8 +83,8 @@ sub normalize {
 		$w2 =~ s/ою$/а/;
 		$w2 =~ s/ой$/а/;
 		$w1 =~ s/ой$/а/;
-		$w1 =~ s/ьею/ья/;
-		$w1 =~ s/ьей/ья/;
+		$w1 =~ s/ею/я/;
+		$w1 =~ s/ей/я/;
 	}
 	
 
@@ -137,7 +139,7 @@ sub transliterate {
 		s/ч\b/ch/g;
 		s/ю/yu/gi;
  
-		tr/йукнгзхфывапролдэсмитьбБЙУКЕНГЗХФЫВАПРОЛДЭСМИТ/jukngzhfyvaproldesmit'bBJUKENGZHFYVAPROLDESMIT/;
+		tr/йукнгзхфывапролдэсмитеьбБЙУКЕНГЗХФЫВАПРОЛДЭСМИТ/jukngzhfyvaproldesmite'bBJUKENGZHFYVAPROLDESMIT/;
 	}
 	
 	
